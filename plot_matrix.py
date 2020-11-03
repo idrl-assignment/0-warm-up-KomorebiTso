97a4063a5685e941a28eec87add8904374833fcc
KomorebiTso <Komorebi0216@163.com>
2020-11-03 21:00:29 +0800

    finish

--- plot_matrix.py
+++ plot_matrix.py
@@ -4,10 +4,8 @@ import matplotlib.image as plt
 def generate_random_matrix(m, n):
     return np.random.randint(0,2,(m,n))
 
-
 def save_matrix(matrix, file_name):
-    plt.imsave(file_name,matrix)
-
+    plt.imsave( file_name,matrix)
 
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
